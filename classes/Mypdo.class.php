6a01765f366188d5abbeb777391a359bf049a50f
MichelCha <michel.chastagner@yahoo.fr>
2018-10-08 09:42:05 +0200

    2018

--- classes/Mypdo.class.php
+++ classes/Mypdo.class.php
@@ -15,7 +15,7 @@ class Mypdo extends PDO
 			$bool=false;
 		}
 		try {
-			$this->dbo =parent::__construct("mysql:host=".DBHOST."; dbname=".DBNAME."; charset=UTF8", DBUSER, DBPASSWD,
+			$this->dbo =parent::__construct("mysql:host=".DBHOST."; dbname=".DBNAME."; port=".DBPORT.";charset=UTF8", DBUSER, DBPASSWD,
 			array(PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => $bool, PDO::ERRMODE_EXCEPTION => $bool));
 			
 		}
--- include/config.inc.php
+++ include/config.inc.php
@@ -7,6 +7,8 @@ define('DBNAME', "covoiturage");
 define('DBUSER', "bd");
 define('DBPASSWD', "bede");
 define('ENV','dev');
+define('SALT','48@!alsd');
+define('DBPORT',3306);
 // pour un environememnt de production remplacer 'dev' (développement) par 'prod' (production)
 // les messages d'erreur du SGBD s'affichent dans l'environememnt dev mais pas en prod
 ?>
