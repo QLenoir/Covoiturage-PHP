a194bde42b9294b468e6effe00310582c0d4951b
MichelCha <michel.chastagner@yahoo.fr>
2018-10-08 09:17:04 +0200

    2018

--- include/pages/accueil.inc.php
+++ include/pages/accueil.inc.php
@@ -1,2 +1,2 @@
-<img class="centreImage" src="image/logo.gif" alt="Covoiturage IUT" title="Covoiturage IUT Limousin" width="38%"/>
+<img class="centreImage" src="image/logo.gif" alt="Covoiturage IUT" title="Covoiturage IUT Limousin" />
 	
