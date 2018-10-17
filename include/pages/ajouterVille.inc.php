f34807d1c3f66973266a6a2bfbcc09864cb3d6f7
Unknown <quentin.lenoir@etu.unilim.fr>
2018-10-17 10:21:58 +0200

    Ajout nom de ville gras + replacement bouton

--- include/pages/ajouterVille.inc.php
+++ include/pages/ajouterVille.inc.php
@@ -4,7 +4,7 @@
 
 	<?php if(empty($_POST['vil_nom'])) { ?>
 	<form action="index.php?page=7" id="NO_COMMANDE" method="post">
-		Nom : <input type="text" name="vil_nom" size="4"><br>
+		<label>Nom : <label><input type="text" name="vil_nom" size="4">
 		<input type=submit value="Valider">
 	</form>
 	<?php } else { 
@@ -14,11 +14,10 @@
 			array('vil_nom' => $_POST['vil_nom'])
 		);
 		if($manager->exists($ville->getVilNom())===true){
-			?><img src="image/erreur.png" alt="Erreur" title="Erreur" /><p>Ajout impossible : ville déja présente</p> <?php
+			?><p><img src="image/erreur.png" alt="Erreur" title="Erreur" /> Ajout impossible : ville déja présente</p> <?php
 		} else {
 			$manager->addVille($ville); ?>
-			<img src="image/valid.png" alt="Validé" title="Validé" /> 
-			<p> La ville "<bold><?php echo $_POST['vil_nom']?></bold>" a été ajoutée</p>
+				<p> <img src="image/valid.png" alt="Validé" title="Validé" /> La ville "<strong><?php echo $_POST['vil_nom']?></strong>" a été ajoutée</p>
 		
 	<?php }
 	} ?>
