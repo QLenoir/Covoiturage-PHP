1e4e8db5a5f161e09a700b184120b40307db8d25
Unknown <quentin.lenoir@etu.unilim.fr>
2018-10-19 13:56:56 +0200

    ajout required pour connexion

--- include/pages/Connexion.inc.php
+++ include/pages/Connexion.inc.php
@@ -4,15 +4,15 @@
 	$manager = new ConnexionManager($db);
 	if(empty($_POST['per_pwd'])) { ?>
 		<form action="index.php?page=11" method="post">
-			<p><label>Nom d'utilisateur : </label></p><input type="text" id="champ" name="per_login" size="4">
-			<p><label>Mot de passe : </label></p><input type="password" id="champ" name="per_pwd" size="4">
+			<p><label>Nom d'utilisateur : </label></p><input type="text" id="champ" name="per_login" size="4" required>
+			<p><label>Mot de passe : </label></p><input type="password" id="champ" name="per_pwd" size="4" required>
 			<?php 
 				$_SESSION['src1'] = rand(1, 9);
 				$_SESSION['src2'] = rand(1, 9);
 			?>
 			<div id="captcha"><img id="nb" src=<?php echo "image/nb/".$_SESSION['src1'] ?> alt=Captcha title="Captcha"  /> <b id ="operation">+</b> 
 			   <img id="nb" src=<?php echo "image/nb/".$_SESSION['src2'] ?> alt=Captcha title="Captcha"  />  <b id="operation">=</b></div>
-			   <input type="text" id="champ" name="reponse" size="4">
+			   <input type="text" id="champ" name="reponse" size="4" required>
 			<input id="connexion" type=submit value="Valider">
 		</form>
 	<?php 
@@ -35,15 +35,15 @@
 				<p> <img src="image/erreur.png" alt="Erreur" title="Erreur" /> CAPTCHA invalide</p>
 			<?php } ?>
 			<form action="index.php?page=11" method="post">
-			<label>Nom d'utilisateur : </label><input type="text" id="champ" name="per_login" size="4">
-			<p><label>Mot de passe : </label><input type="password" id="champ" name="per_pwd" size="4"></p><br>
+			<label>Nom d'utilisateur : </label><input type="text" id="champ" name="per_login" size="4" required>
+			<p><label>Mot de passe : </label><input type="password" id="champ" name="per_pwd" size="4" required></p><br>
 			<?php 
 				$_SESSION['src1'] = rand(1, 9);
 				$_SESSION['src2'] = rand(1, 9);
 			?>
 			<div id="captcha"><img id="nb" src=<?php echo "image/nb/".$_SESSION['src1'] ?> alt=Captcha title="Captcha"  /> <b id ="operation">+</b> 
 			   <img id="nb" src=<?php echo "image/nb/".$_SESSION['src2'] ?> alt=Captcha title="Captcha"  />  <b id="operation">=</b></div>
-			   <input type="text" id="champ" name="reponse" size="4">
+			   <input type="text" id="champ" name="reponse" size="4" required>
 			<input id="connexion" type=submit value="Valider">
 			</form>
 		<?php }
