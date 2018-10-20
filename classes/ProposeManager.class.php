4fbd4703815a0ebf6a7a23897c65052f6eafae20
Kant1 <quentin.lenoir@etu.unilim.fr>
2018-10-20 17:36:55 +0200

    Ajout 1er écran rechercher trajet

--- classes/ProposeManager.class.php
+++ classes/ProposeManager.class.php
@@ -25,4 +25,26 @@ class ProposeManager{
 		$pers = new Personne($res);
 		return $pers->getPNum();
 	}
+
+	public function getVilleDepart() {
+		$req = $this->db->prepare('SELECT vil_num1 AS vil_num FROM parcours pa JOIN propose po ON pa.par_num=po.par_num WHERE pro_sens=0 UNION SELECT vil_num2 AS vil_num FROM parcours pa JOIN propose po ON pa.par_num=po.par_num WHERE pro_sens=1;');
+		$req->execute();
+
+		while ($ville = $req->fetch(PDO::FETCH_OBJ)) {
+			$listeVilles[] = new Ville ($ville);
+
+		}
+		var_dump($listeVilles);
+		return $listeVilles;
+
+		$req->closeCursor();
+	}
+
+	public function recupNomVille($numVille) {
+		$req = $this->db->prepare('SELECT vil_nom FROM ville WHERE vil_num="'.$numVille.'";');
+		$req->execute();
+		$res = $req->fetch(PDO::FETCH_OBJ);
+		$ville = new Ville($res);
+		return $ville->getVilNom();
+	}
 }
--- include/pages/ChercherTrajet.inc.php
+++ include/pages/ChercherTrajet.inc.php
@@ -1,4 +1,19 @@
 <?php if(!isset($_SESSION['login'])) {
 	header('Location: index.php?page=0');
-} ?>
+	}
+	$db = new Mypdo();
+	$manager = new ProposeManager($db);
+ ?>
 <h1>Rechercher un trajet</h1>
+
+<form action="index.php?page=10" id="vil_nom" method="post">
+	<p><label>Ville de départ : </label></p>
+	<p><select id="champ" size="1" name="vil_num1" >
+	<option value="0" > Choisissez </option>
+	<?php $listeParcours = $manager->getVilleDepart(); 
+		foreach ($listeParcours as $attribut => $value) { ?>
+				<option value= <?php echo $value->getVilNum() ?> > <?php echo $manager->recupNomVille($value->getVilNum()) ?> </option>
+	<?php } ?>
+	</select></p>
+	<p><input id="valider" type=submit value="Valider"></p>
+</form>
