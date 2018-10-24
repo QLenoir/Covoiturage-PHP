2d82ba61453e29554b122df1d64c39d023cab958
Lara <lv.laravidoni@gmail.com>
2018-10-24 11:47:46 +0200

    Classes Etu/Sal

--- classes/Etudiant.class.php
+++ classes/Etudiant.class.php
@@ -1,4 +1,46 @@
 <?php
 class Etudiant{
-	
+	private $etu_num;
+	private $dep_num;
+	private $div_num;
+
+	public function __construct($valeurs) {
+		if (!empty($valeurs)) {
+			$this -> affecte($valeurs);
+		}
+	}
+
+	public function affecte($donnees){
+		foreach ($donnees as $attribut =>$valeur){
+
+			switch ($attribut){
+				case 'per_num' : $this->setEtuNum($valeur);
+					break;
+				case 'dep_num' : $this->setDepNum($valeur);
+					break;
+				case 'div_num' : $this->setDivNum($valeur);
+					break;
+				
+			}
+		}
+	}
+	public function setEtuNum($num) {
+		$this->etu_num = $num;
+	}
+	public function setDepNum($num) {
+		$this->dep_num = $num;
+	}
+	public function setDivNum($num){
+		$this->div_num = $num;
+	}
+
+	public function getEtuNum() {
+		return $this->etu_num;
+	}
+	public function getDepNum() {
+		return $this->dep_num;
+	}
+	public function getDivNum() {
+		return $this->div_num;
+	}
 }
--- classes/Salarie.class.php
+++ classes/Salarie.class.php
@@ -1,4 +1,46 @@
 <?php
 class Salarie{
-	
+	private $sal_num;
+	private $sal_tel;
+	private $fon_num;
+
+	public function __construct($valeurs) {
+		if (!empty($valeurs)) {
+			$this -> affecte($valeurs);
+		}
+	}
+
+	public function affecte($donnees){
+		foreach ($donnees as $attribut =>$valeur){
+
+			switch ($attribut){
+				case 'sal_num' : $this->setSalNum($valeur);
+					break;
+				case 'sal_telprof' : $this->setSalTel($valeur);
+					break;
+				case 'fon_num' : $this->setFonNum($valeur);
+					break;
+				
+			}
+		}
+	}
+	public function setSalNum($num) {
+		$this->sal_num = $num;
+	}
+	public function setSalTel($num) {
+		$this->sal_tel = $num;
+	}
+	public function setFonNum($num){
+		$this->fon_num = $num;
+	}
+
+	public function getSalNum() {
+		return $this->sal_num;
+	}
+	public function getSalTel() {
+		return $this->sal_tel;
+	}
+	public function getDivNum() {
+		return $this->fon_num;
+	}
 }
