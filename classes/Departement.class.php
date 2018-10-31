60505442d30590fc48aa295eecc98f2fec43fd0c
Lara <lv.laravidoni@gmail.com>
2018-10-31 23:22:24 +0100

    Fin classes

--- classes/Departement.class.php
+++ classes/Departement.class.php
@@ -1,4 +1,47 @@
 <?php
 class Departement{
-	
+
+	private $dep_num;
+	private $dep_nom;
+	private $vil_num;
+
+
+	public function __construct($valeurs) {
+		if (!empty($valeurs)) {
+			$this -> affecte($valeurs);
+		}
+	}
+
+	public function affecte($donnees){
+		foreach ($donnees as $attribut =>$valeur){
+
+			switch ($attribut){
+				case 'dep_num' : $this->setDNum($valeur);
+					break;
+				case 'dep_nom' : $this->setDNom($valeur);
+					break;
+				case 'vil_num' : $this->setVNum($valeur);
+					break;
+			}
+		}
+	}
+	public function setDNum($num) {
+		$this->dep_num = $num;
+	}
+	public function setDNom($nom) {
+		$this->dep_nom = $nom;
+	}
+	public function setVNum($vilNum){
+		$this->vil_num = $vilNum;
+	}
+
+	public function getDNum() {
+		return $this->dep_num ;
+	}
+	public function getDNom() {
+		return $this->dep_nom;
+	}
+	public function getVNum(){
+		return $this->vil_num;
+	}
 }
--- classes/Division.class.php
+++ classes/Division.class.php
@@ -1,4 +1,36 @@
 <?php
 class Division{
-	
+	private $div_num;
+	private $div_nom;
+
+
+	public function __construct($valeurs) {
+		if (!empty($valeurs)) {
+			$this -> affecte($valeurs);
+		}
+	}
+
+	public function affecte($donnees){
+		foreach ($donnees as $attribut =>$valeur){
+
+			switch ($attribut){
+				case 'div_num' : $this->setDNum($valeur);
+					break;
+				case 'div_nom' : $this->setDNom($valeur);
+					break;
+			}
+		}
+	}
+	public function setDNum($num) {
+		$this->dep_num = $num;
+	}
+	public function setDNom($nom) {
+		$this->dep_nom = $nom;
+	}
+	public function getDNum() {
+		return $this->div_num ;
+	}
+	public function getDNom() {
+		return $this->div_nom;
+	}
 }
